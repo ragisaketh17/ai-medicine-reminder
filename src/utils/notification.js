456a8b1b7aedbaba9d238f6fb95c12da8c958c84
ragisaketh17 <ragisaketh17@gmail.com>
2026-07-12 15:13:11 +0530

    Remove notification icon

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -1,22 +1,13 @@
 export const requestNotificationPermission = async () => {
-  if (!("Notification" in window)) {
-    alert("This browser doesn't support notifications.")
-    return
-  }
+  if (!("Notification" in window)) return
 
   const permission = await Notification.requestPermission()
 
-  if (permission === "granted") {
-    console.log("Notification permission granted")
-  } else {
-    console.log("Notification permission denied")
-  }
+  console.log("Permission:", permission)
 }
 
 export const showNotification = medicine => {
-  if (Notification.permission !== "granted") {
-    return
-  }
+  if (Notification.permission !== "granted") return
 
   new Notification("💊 Medicine Reminder", {
     body: `Time to take ${medicine.name}\nDosage: ${medicine.dosage}`,
